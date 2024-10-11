d9c2ddd3e41807aba112b38cd8a0b3f260236d83
Chirag <chiragaggarwal51@yahoo.com>
2024-10-11 15:56:12 +0530

    pagination

--- searchSortTable/index.js
+++ searchSortTable/index.js
@@ -1,36 +1,78 @@
 let tablebody = document.querySelector("#userTable tbody");
 let users = [];
+let updatedUser = [];
 let nameSortOrder = `asc`;
-
+let pageSize = 5;
+let currentPage = 1;
+let loading = document.getElementById("loading");
 async function fetchUsers() {
   try {
+    loading.style.display = "block";
     const response = await fetch("https://dummyjson.com/users");
     const data = await response.json();
     users = data.users;
+    updatedUser = users;
     renderTable(users);
+    renderPagination(users);
   } catch (err) {
     console.error("Error fetching data:", err);
+  } finally {
+    loading.style.display = "none";
   }
 }
 fetchUsers(); //Use init function
 
 function renderTable(users) {
   while (tablebody.firstChild) {
-    tablebody.removeChild(tablebody.firstChild);//used to remove child element
+    tablebody.removeChild(tablebody.firstChild);
   }
-  users.forEach((user, index) => {
-    let color = index % 2 == 0 ? "orange" : "red";
+
+  // Determine the start and end indexes for pagination
+  let start = (currentPage - 1) * pageSize;
+  let end = start + pageSize;
+  let paginatedUsers = users.slice(start, end); //exclude element at end index
+  console.log(paginatedUsers);
+  paginatedUsers.forEach((user, index) => {
+    let color = index % 2 === 0 ? "orange" : "red";
     let row = `<tr class=${color}>
         <td>${user.firstName + " " + user.lastName}</td>
-        <td>${user.age}</td>
+        <td contenteditable="true" onblur="updateAge(${users.indexOf(
+          user
+        )}, this
+        )">${user.age}</td>
     </tr>`;
-    tablebody.insertAdjacentHTML("beforeend", row); //"beforeend": Just inside the element, after its last child.
+    tablebody.insertAdjacentHTML("beforeend", row);
   });
+}
+
+function updateAge(index, element) {
+  let newAge = parseInt(element.innerText);
+  if (!isNaN(newAge) && newAge > 0) {
+    users[index].age = newAge;
+  } else {
+    element.innerText = users[index].age;
+  }
+}
+
+function myFunction(i) {
+  currentPage = i;
+  renderTable(updatedUser);
+}
+
+function renderPagination(user) {
+  pagination.innerHTML = "";
+  const paginationElement = document.getElementById("pagination");
+
+  const pageCount = Math.ceil(user.length / pageSize);
+  for (i = 1; i <= pageCount; i++) {
+    let pageButton = `<button onclick="myFunction(${i})">${i}</button>`;
+    pagination.insertAdjacentHTML("beforeend", pageButton);
+  }
 }
 
 function searchUser(e) {
   let searchInput = e.target.value.toLowerCase();
-  let updatedUser = [];
+  updatedUser = [];
   users.forEach((user) => {
     let name = `${user.firstName.toLowerCase() + user.lastName.toLowerCase()}`;
     if (name.includes(searchInput)) {
@@ -38,7 +80,9 @@ function searchUser(e) {
       updatedUser.push(user);
     }
   });
+  currentPage = 1;
   renderTable(updatedUser);
+  renderPagination(updatedUser);
 }
 
 function sortUsers(order) {
@@ -55,8 +99,18 @@ function sortByName() {
   nameSortOrder = nameSortOrder === "asc" ? "desc" : "asc";
 }
 
-document.getElementById("search").addEventListener("keyup", searchUser);
-document.getElementById("nameHeader").addEventListener("click", sortByName);
+let search = document.getElementById("search");
+let sortName = document.getElementById("nameHeader");
+let pagination = document.getElementById("pagination");
+let page = document.getElementById("pageSize");
+page.addEventListener("change", (e) => {
+  pageSize = parseInt(e.target.value);
+  renderPagination(updatedUser);
+  renderTable(updatedUser);
+});
+
+search.addEventListener("keyup", searchUser);
+sortName.addEventListener("click", sortByName);
 
 //get tbody
 //get users data using async await in [json] format
@@ -64,9 +118,8 @@ document.getElementById("nameHeader").addEventListener("click", sortByName);
 //search-->check whether name.includes(e.target.value) if yes than push that array in updated array & renderTable(updatedArrayUser)
 //sort-->sortbyname get called first--> calls sort(order) & reverse the order and then .in sort(order) user.sort(a,b)
 //a-b for asc ,b-a for desc
-
+//pageSize-->(renderPagination(user)-->pageCount-->button add --> function --> current page =i renderTable(user)  )--> renderTable-->start,end,paginated user,loop,element add
 //loading
 //sorting by age
 //paginated view -->page size 5,10,15 with dropdown
-//edit option in age -->input field 
-
+//edit option in age -->input field
